a8c25af4b7b9257f2d4a3e3d287edffb84c4237d
azaya <ann.marie.zaya@gmail.com>
2018-12-08 22:30:58 -0600

    Part 2 HW

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,60 @@
 import os
-import pandas as pd 
+import csv
 
-electioncsv = os.path.join("PyPoll","election_data.csv")
+electioncsv = os.path.join("PyPoll", "election_data.csv")
 
+voters = []
+candidate = []
+candidate_list = []
 
-election_data = pd.read_csv(electioncsv)
+Khan = 0
+Correy = 0
+Li = 0
+OTooley = 0
+
+with open (electioncsv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
+    for row in csvreader:
+        voters.append(row[0])
+        candidate.append(row[2])
+        total_votes = len(voters)
+    for i in candidate:
+        if i not in candidate_list:
+            candidate_list.append(i)
+        elif i == 'Khan':
+            Khan = Khan + 1
+        elif i == 'Correy':
+            Correy = Correy + 1
+        elif i == 'Li':
+            Li = Li + 1
+        elif i == "O'Tooley":
+            OTooley = OTooley + 1
+        Khan_percent = Khan/total_votes
+        Correy_percent = Correy/total_votes
+        Li_percent =Li/total_votes    
+        OTooley_percent = OTooley/total_votes
+
+total_votes = total_votes 
+candidate_list = candidate_list 
+candidates = candidate 
+
+Khan_percent = (Khan/total_votes)*100
+Correy_percent = (Correy/total_votes)*100
+Li_percent = (Li/total_votes)*100
+OTooley_percent = (OTooley/total_votes)*100
+
+Election_Analysis = [Khan, Correy, Li, OTooley]
+Winner = max(Election_Analysis)
+
 
 print("Election Results")
-print("----------------------------")
-
-total_votes = len(election_data["Voter ID"].value_counts())
-print(f'Total Votes: {total_votes}')
+print("------------------------")
+print("Total Votes: " + str(total_votes))
+print("------------------------")
+print("Khan: " + str('{:.3f}'.format(round(Khan_percent, 3)))+ "% " + "(" + str(Khan) + ")")
+print("Correy: " + str('{:.3f}'.format(round(Correy_percent, 3)))+ "% " + "(" + str(Correy) + ")")
+print("Li: " + str('{:.3f}'.format(round(Li_percent, 3))) + "% " + "(" + str(Li) + ")")
+print("O'Tooley: " + str('{:.3f}'.format(round(OTooley_percent, 3))) + "% " + " (" + str(OTooley) + ")")
+print("------------------------")
+print("Winner: " + str(candidate_list[Election_Analysis.index(max(Election_Analysis))]))
